d31e4a457533d9af91660653bb9061d1dce85855
arimura <kotaro_arimura@voyagegroup.com>
2019-12-10 21:23:15 +0900

    Proto

--- chapter2-1-1/ATC001A/main.go
+++ chapter2-1-1/ATC001A/main.go
@@ -11,56 +11,65 @@ import (
 
 func main() {
 	y, x, c := handleInput()
-	fmt.Printf("%v, %v, %v\n", y, x, c)
-	if dfs(y, x, c) {
+	stack := make([]string, 0)
+	if dfs(y, x, c, &stack) {
 		fmt.Println("Yes")
 		return
 	}
 	fmt.Println("No")
 }
 
-func inStack(y, x int, stack []string) bool {
-
+func push(y, x int, stack *[]string) {
+	v := strconv.Itoa(y) + "-" + strconv.Itoa(x)
+	*stack = append(*stack, v)
 }
 
-func dfs(y, x int, c [][]string) bool {
-	fmt.Printf("y:%d, x:%d\n", y, x)
+func pop(stack *[]string) {
+	*stack = (*stack)[:len(*stack)-1]
+}
 
+func inStack(y, x int, stack []string) bool {
+	v := strconv.Itoa(y) + "-" + strconv.Itoa(x)
+	for _, e := range stack {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
+
+func dfs(y, x int, c [][]string, stack *[]string) bool {
 	if c[y][x] == "g" {
 		return true
 	}
-	fmt.Println("assign")
-	c[y][x] = "X"
+	push(y, x, stack)
 
 	//go up
-	fmt.Println("up")
-	if 0 < y && c[y-1][x] == "." {
-		if dfs(y-1, x, c) {
+	if 0 < y && c[y-1][x] != "#" && !inStack(y-1, x, *stack) {
+		if dfs(y-1, x, c, stack) {
 			return true
 		}
 	}
 	//go down
-	fmt.Println("down")
-	if y < len(c)-1 && c[y+1][x] == "." {
-		if dfs(y+1, x, c) {
+	if y < len(c)-1 && c[y+1][x] != "#" && !inStack(y+1, x, *stack) {
+		if dfs(y+1, x, c, stack) {
 			return true
 		}
 	}
 	//go left
-	fmt.Println("left")
-	if 0 < x && c[y][x-1] == "." {
-		if dfs(y, x-1, c) {
+	if 0 < x && c[y][x-1] != "#" && !inStack(y, x-1, *stack) {
+		if dfs(y, x-1, c, stack) {
 			return true
 		}
 	}
 	//go right
-	fmt.Println("right")
-	if x < len(c[0])-1 && c[y][x+1] == "." {
-		if dfs(y, x+1, c) {
+	if x < len(c[0])-1 && c[y][x+1] != "#" && !inStack(y, x+1, *stack) {
+		if dfs(y, x+1, c, stack) {
 			return true
 		}
 	}
 
+	pop(stack)
 	return false
 }
 
