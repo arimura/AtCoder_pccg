98df8a12e14b8cb05e4e6941e43dbee0b1bc9c75
arimura <33759663+arimura@users.noreply.github.com>
2019-12-14 10:25:36 +0900

    Prototype

--- chapter2-1-2/ARC031B/main.go
+++ chapter2-1-2/ARC031B/main.go
@@ -12,88 +12,113 @@ type point struct {
 }
 
 func main() {
-	// fmt.Println())
-	landMap, lands := handleInput()
+	landMap, _ := handleInput()
 
 	var landChecked [][]bool
 	for _, row := range landMap {
 		landChecked = append(landChecked, make([]bool, len(row)))
 	}
 
-	//dequeue
-	land := lands[0]
-	lands = lands[1:]
-
-	var continuousLand []point
-	dfs(landMap, landChecked, &lands, &continuousLand, land.y, land.x)
-	fmt.Printf("lands: %v\n", lands)
-	fmt.Printf("continuousLand: %v\n", continuousLand)
-
-	land = dequeueAdjecentLand(&landMap, &lands, &continuousLand)
-	fmt.Printf("lands: %v\n", land)
-	dfs(landMap, landChecked, &lands, &continuousLand, land.y, land.x)
-	fmt.Printf("lands: %v\n", lands)
-	fmt.Printf("continuousLand: %v\n", continuousLand)
-
-	if len(lands) == 0 {
-		fmt.Println("Yes")
-		return
-	}
-	fmt.Println("No")
-}
-
-func dequeueAdjecentLand(landMap *[][]rune, lands, continuousLand *[]point) point {
-	var f point
-	for _, partOfContinuousLand := range *continuousLand {
-		for i, land := range *lands {
-			//check land is arouncd partOfContinuousLand
-			y := partOfContinuousLand.y - land.y
-			if y < 0 {
-				y = -y
-			}
-			x := partOfContinuousLand.x - land.x
-			if x < 0 {
-				x = -x
-			}
-
-			if y+x == 2 {
-				//found
-				f = (*lands)[i]
-				*lands = append((*lands)[:i], (*lands)[i+1:]...)
-
-				(*landMap)[partOfContinuousLand.y-((partOfContinuousLand.y-land.y)/2)][partOfContinuousLand.x-((partOfContinuousLand.x-land.x)/2)] = 'o'
-				fmt.Printf("new mapped x: %d, y %d", partOfContinuousLand.x-((partOfContinuousLand.x-land.x)/2), partOfContinuousLand.y-((partOfContinuousLand.y-land.y)/2))
-				return f
+	var x, y int
+	for yIdx, row := range landMap {
+		for xIdx, v := range row {
+			if v == 'o' {
+				y = yIdx
+				x = xIdx
+				break
 			}
 		}
 	}
-	return f
-}
 
-func dfs(landMap [][]rune, landChecked [][]bool, lands, continuousLand *[]point, y, x int) {
-	if y < 0 || x < 0 || len(landMap)-1 < y || len(landMap[0])-1 < x || landMap[y][x] == 'x' || landChecked[y][x] {
-		//no land
+	f := false
+	if dfs(landMap, &landChecked, y, x, &f) {
+		fmt.Println("YES")
 		return
 	}
+	fmt.Println("NO")
+}
 
-	landChecked[y][x] = true
-
-	//found land.
-	//remove from lands
-	for i, land := range *lands {
-		if land.y == y && land.x == x {
-			*lands = append((*lands)[:i], (*lands)[i+1:]...)
-			break
+func uncheckedLandNum(landMap [][]rune, landChecked *[][]bool) int {
+	n := 0
+	for y, row := range landMap {
+		for x, v := range row {
+			if v == 'o' && !(*landChecked)[y][x] {
+				n++
+			}
 		}
 	}
-	//add to continuousLand
-	*continuousLand = append(*continuousLand, point{y, x})
+	return n
+}
 
-	//up, down, left, rigth
-	dfs(landMap, landChecked, lands, continuousLand, y+1, x)
-	dfs(landMap, landChecked, lands, continuousLand, y-1, x)
-	dfs(landMap, landChecked, lands, continuousLand, y, x-1)
-	dfs(landMap, landChecked, lands, continuousLand, y, x+1)
+func copy(src [][]bool) [][]bool {
+	copied := make([][]bool, 0)
+	for _, row := range src {
+		copiedRow := make([]bool, 0)
+		for _, v := range row {
+			copiedRow = append(copiedRow, v)
+		}
+		copied = append(copied, copiedRow)
+	}
+	return copied
+}
+
+func dfs(landMap [][]rune, landChecked *[][]bool, y, x int, useFilling *bool) bool {
+	//check unchecked land exists
+	n := uncheckedLandNum(landMap, landChecked)
+	if n == 0 {
+		return true
+	}
+
+	if y < 0 || x < 0 || len(landMap)-1 < y || len(landMap[0])-1 < x || (*landChecked)[y][x] {
+		//out of range or alreadu checked
+		return false
+	}
+	//use filling
+	if landMap[y][x] == 'x' && !*useFilling {
+		fmt.Printf("on y:%d, x:%d by fullfilling\n", y, x)
+
+		copied := copy(*landChecked)
+
+		(*landChecked)[y][x] = true
+		*useFilling = true
+		if dfs(landMap, &copied, y+1, x, useFilling) {
+			return true
+		}
+		if dfs(landMap, &copied, y-1, x, useFilling) {
+			return true
+		}
+		if dfs(landMap, &copied, y, x-1, useFilling) {
+			return true
+		}
+		if dfs(landMap, &copied, y, x+1, useFilling) {
+			return true
+		}
+		*useFilling = false
+		(*landChecked)[y][x] = false
+		return false
+	}
+
+	if landMap[y][x] == 'x' {
+		//do nothing
+		return false
+	}
+
+	//on land
+	fmt.Printf("on y:%d, x:%d\n", y, x)
+	(*landChecked)[y][x] = true
+	if dfs(landMap, landChecked, y+1, x, useFilling) {
+		return true
+	}
+	if dfs(landMap, landChecked, y-1, x, useFilling) {
+		return true
+	}
+	if dfs(landMap, landChecked, y, x-1, useFilling) {
+		return true
+	}
+	if dfs(landMap, landChecked, y, x+1, useFilling) {
+		return true
+	}
+	return false
 }
 
 // handleInput return 2 dimensions rune([y][x]rune) .
