0113326acfe0beaf26407834f3249e63374ff9bb
arimura <kotaro_arimura@voyagegroup.com>
2020-03-03 07:48:15 +0900

    Use memo on AOJ-Course-0-1

--- chapter2-3-1/AOJ-Course-0-1/main.go
+++ chapter2-3-1/AOJ-Course-0-1/main.go
@@ -13,14 +13,14 @@ var (
 	n      int
 	v      [1000]int
 	w      [1000]int
-	memo   [100][10000]int
+	memo   [1000][100000]int
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	t := strings.Split(scanner.Text(), " ")
-	n, _ := strconv.Atoi(t[0])
+	n, _ = strconv.Atoi(t[0])
 	totalW, _ = strconv.Atoi(t[1])
 
 	i := 0
@@ -30,25 +30,28 @@ func main() {
 		w[i], _ = strconv.Atoi(t[1])
 		i++
 	}
+	// maxV := 0
+	fmt.Println(search(0, totalW))
+}
 
-	maxV := 0
-	un := uint(n)
-	for bit := 0; bit < (1 << un); bit++ {
-		fmt.Printf("bit: %04b\n", bit)
-		tmpW := 0
-		tmpV := 0
-		for d := n - 1; 0 <= d; d-- {
-			if 0 < bit&(1<<uint(d)) {
-				if tmpW+w[d] <= totalW {
-					tmpW += w[d]
-					tmpV += v[d]
-				}
-			}
-		}
-		fmt.Println(tmpV)
-		if maxV < tmpV {
-			maxV = tmpV
-		}
+func search(i, u int) int {
+	if memo[i][u] != 0 {
+		return memo[i][u]
 	}
-	fmt.Println(maxV)
+
+	if i == n {
+		return 0
+	} else if u < w[i] {
+		return search(i+1, u)
+	} else {
+		r1 := search(i+1, u)
+
+		r2 := search(i+1, u-w[i]) + v[i]
+		if r1 < r2 {
+			memo[i][u] = r2
+			return r2
+		}
+		memo[i][u] = r1
+		return r1
+	}
 }
